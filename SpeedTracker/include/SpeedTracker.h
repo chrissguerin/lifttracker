13134649d54034b1376cb641c7816061497f9153
Christophe Guerin <christophe.guerin.1@ulaval.ca>
2024-11-20 03:20:04 -0500

    init project

---
+++ SpeedTracker/include/SpeedTracker.h
@@ -0,0 +1,13 @@
+#ifndef SPEED_TRACKER_H // include guard
+#define SPEED_TRACKER_H
+
+namespace LiftTracker
+{
+    class SpeedTracker{
+        public:
+        private:
+    };
+}
+
+
+#endif /*SPEED_TRACKER_H*/
